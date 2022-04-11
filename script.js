79db540bcbd89815af49dd320b546199e973c9f9
Digital-Access <102788087+Digital-Access@users.noreply.github.com>
2022-04-11 16:15:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,13 @@
-let titleScreen = 'Multiple Choice Quiz';
+let titleScreen = 'Knowledge Check!';
 let allQuestions = [{
-    question_string: "Which colour isn't the sky",
-    correct_text: "✔ Correct! The sky doesn't contain shades of green.",
-    incorrect_text: "❌Incorrect! The colour the sky doesn't contain is green.",
+    question_string: "What are the 3 main types of social audience",
+    correct_text: "😁Correct!",
+    incorrect_text: "❌Incorrect! The correct answer is Custom, Lookalike and Saved.",
     choices: {
-        all_choices: ["Pink", "Blue", "Orange", "Green"],
-        correct: "Green",
+        all_choices: ["Custom, Potential and Partner", "Custom, Lookalike and Saved", "Conceptual, Lookalike and Syndicated"],
+        correct: "Custom, Lookalike and Saved"
     }
-}, {
-    question_string: "Which of the following elements aren’t introduced in HTML5?",
-    correct_text: "✔ Correct! <hgroup> is not included in HTML5.",
-    incorrect_text: "❌Incorrect! <hgroup> is the element not introduced in HTML5.",
-    choices: {
-        all_choices: ["<hgroup>", "<article>", "<footer>", "<input>"],
-        correct: "<hgroup>",
-    }
-}, {
-    question_string: "How many wheels are there on a tricycle?",
-    correct_text: "✔ Correct! Tri is a combining noun, meaning three; having three.",
-    incorrect_text: "❌Incorrect! A tricycle has three wheels.",
-    choices: {
-        all_choices: ["Four", "One", "Two", "Three"],
-        correct: "Three",
-    }
-}, {
-    question_string: 'Who is the main character of Harry Potter?',
-    correct_text: "✔ Correct! Harry is the protagonist.",
-    incorrect_text: "❌Incorrect! Harry Potter is the main character (hint: It says it in the title).",
-    choices: {
-        all_choices: ["Harry Potter", "Ron Weasley", "Hermione Granger", "Voldemort"],
-        correct: "Harry Potter",
-    }
-}, {
-    question_string: 'Whats 3X4?',
-    correct_text: "✔ Correct! 3X4 is 12.",
-    incorrect_text: "❌Incorrect! 12 is the correct answer.",
-    choices: {
-        all_choices: ["7", "11", "22", "12"],
-        correct: "12",
-    }
+}
 }];
 
 
